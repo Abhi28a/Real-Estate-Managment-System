045bfd2c9eaf8458a45203514fd1b1743fb32f20
Abhi28a <1289saggam.gmail.com>
2022-12-05 02:06:32 +0530

    project

---
+++ server/config/config.dev.js
@@ -0,0 +1,5 @@
+// Development keys here!!
+module.exports = {
+  secret: 'h3ll0w0rl6',
+  mongoURI: 'mongodb+srv://Realestate:Abhiabhishek@cluster0.atp6i0a.mongodb.net/?retryWrites=true&w=majority'
+};
